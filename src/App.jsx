071e86f156345b18ba433dc33a33f16c9a02093b
angela1719 <angelarm783@gmail.com>
2024-03-26 12:46:30 -0500

    componente 100% reutilizable Twitter

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { TwitterFollowCard } from './TwitterFollowCard'
 
  export function App () {
  return (
-    <>
+    <section className='App'>
     <TwitterFollowCard userName= "Angelita1719" name= "Angela Ramírez" />
     <TwitterFollowCard userName= "Diegosernavs1" name= "Diego Serna" />
-    </>
+    <TwitterFollowCard userName= "Lory07" name= "Laura Ramírez" />
+    <TwitterFollowCard userName= "Nana31" name= "Ana María Ramírez" />
+    </section>
  )
 }
