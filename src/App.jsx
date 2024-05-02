c42cbe323e2f8a08b0a12f9db634085e9dda22ad
angela1719 <angelarm783@gmail.com>
2024-05-02 11:47:51 -0500

    avance formatUserName

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
  import './App.css'
-import { TwitterFollowCard } from './TwitterFollowCard'
+import { TwitterFollowCard } from './TwitterFollowCard.jsx'
 
  export function App () {
+   const formatUserName = (userName) => `@${userName}`
  return (
     <section className='App'>
-    <TwitterFollowCard userName= "Angelita1719" name= "Angela Ramírez" />
-    <TwitterFollowCard userName= "Diegosernavs1" name= "Diego Serna" />
-    <TwitterFollowCard userName= "Lory07" name= "Laura Ramírez" />
-    <TwitterFollowCard userName= "Nana31" name= "Ana María Ramírez" />
+    <TwitterFollowCard formatUserName={formatUserName} isFollowing={true} userName= "Angelita1719" name= "Angela Ramírez" />
+    <TwitterFollowCard formatUserName={formatUserName} isFollowing={false} userName= "Diegosernavs1" name= "Diego Serna" />
+    <TwitterFollowCard formatUserName={formatUserName} isFollowing userName= "Lory07" name= "Laura Ramírez" />
+    <TwitterFollowCard formatUserName={formatUserName} isFollowing userName= "Nana31" name= "Ana María Ramírez" />
     </section>
  )
 }
